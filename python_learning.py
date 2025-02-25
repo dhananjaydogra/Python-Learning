88ff1330da6650a476ccf37e5f8755a7a4660a83
Dhananjay Dogra <dhananjay27dogra@gmail.com>
2025-02-25 21:10:18 +0530

    Added RollerCoster ticket price calculator

--- python_learning.py
+++ python_learning.py
@@ -34,3 +34,34 @@
 # result=(bill+(bill*(tip_percent/100.0)))/split
 # print(f"\nEach person should pay: ${round(result,2)}")
 
+#Even Odd Number using If constarint
+# number = int(input("Enter a number: "))
+
+# if (number % 2 == 0):
+#      print (f"{number} is an Even number.") 
+# else:
+#      print (f"{number} is an Odd number.")
+
+#Ticketing system for a rollercoster
+
+print("\nWelcome to the Rollercoster!\n") 
+age = int(input("Enter your age: ")) 
+height = int(input("Enter your height: ")) 
+bill=0
+if (height > 120):
+    print("You can ride it!")
+    if (age < 12):
+        bill= 5
+        print("Child Tickets are $5")
+    elif (age >= 18):
+        bill= 12
+        print("Adult Tickets are $12")
+    else:
+        bill= 7
+        print("Youth Tickets are $12")
+    photo = input("Want to make the ride memorable with a photograph? Type yes or no.  ")
+    if (photo=="yes"):  
+        bill += 3
+    print(f"Your Final Bill is ${bill}")
+else:
+    print("Sorry, your height doesn't meet the eligibility to take the ride.")
