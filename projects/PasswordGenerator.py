2ea87121047ae98735191b397811f9062de3d2f2
Dhananjay Dogra <dhananjay27dogra@gmail.com>
2025-02-28 02:09:12 +0530

    Added an easier meathod for generating passwords

--- projects/PasswordGenerator.py
+++ projects/PasswordGenerator.py
@@ -12,20 +12,31 @@ nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 passwordlist = []
 
+# for letter in range(0,nr_letters):
+#     passwordlist.append(letters[random.randint(0,len(letters)-1)])
 
-for letter in range(0,nr_letters):
-    passwordlist.append(letters[random.randint(0,len(letters)-1)])
+# for symbol in range(0,nr_symbols):
+#     passwordlist.append(symbols[random.randint(0,len(symbols)-1)])
 
-for symbol in range(0,nr_symbols):
-    passwordlist.append(symbols[random.randint(0,len(symbols)-1)])
+# for number in range(0, nr_numbers):
+#     passwordlist.append(numbers[random.randint(0, len(numbers) - 1)])
 
-for number in range(0, nr_numbers):
-    passwordlist.append(numbers[random.randint(0, len(numbers) - 1)])
+##Easier Alternative
+for char in range(0,nr_letters):
+    passwordlist+=random.choice(letters)
+
+for char in range(0,nr_symbols):
+    passwordlist+=random.choice(symbols)
+
+for char in range(0,nr_numbers):
+    passwordlist+=random.choice(numbers)
 
 print(passwordlist)
 random.shuffle(passwordlist)
 print(passwordlist)
 password=""
+
 for passletter in passwordlist:
     password=password + passletter
+    
 print(f"Your password is : {password}")
