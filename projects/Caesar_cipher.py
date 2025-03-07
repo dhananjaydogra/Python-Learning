735989147098e994e4aaf9d16232d83a6cbf1937
dhananjaydogra <dhananjay27dogra@gmail.com>
2025-03-07 22:09:40 +0530

    Caesar Cipher progam which will encode/decode only alphabet while ignoring other characters

---
+++ projects/Caesar_cipher.py
@@ -0,0 +1,66 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+print(logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    if encode_or_decode == "decode":
+        shift_amount *= -1
+
+    for letter in original_text:
+        if letter not in alphabet:
+            output_text +=letter
+        else:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+#Another logic
+def caesar_other(original_text,shift_amount,work_mode):
+    coded_text = ""
+    change=1
+    length=len(alphabet)
+    if work_mode == "decode":
+        change = -1
+    for letter in original_text:
+        if letter not in alphabet:
+            coded_text += letter
+        else:
+            shifted_index = alphabet.index(letter) + (shift_amount * change)
+            while shifted_index < 0 or shifted_index > (length - 1):
+                shifted_index = shifted_index + (-length * change)
+            coded_text += alphabet[shifted_index]
+    print(f"Here is the {work_mode}d result : {coded_text} ")
+
+
+check=""
+while check != "no":
+
+    direction = input("\nType 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    if direction=="encode" or direction=="decode":
+        caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+    else:
+        print("\n Please check the Direction entererd.")
+    #caesar_other(original_text=text, shift_amount=shift, work_mode=direction)
+    check = input("Type no to exit or anything else to continue : ").lower()
