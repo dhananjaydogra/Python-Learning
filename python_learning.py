c24cb78d36f593d1a61d9c59cc436c928027dbac
dhananjaydogra <dhananjay27dogra@gmail.com>
2025-03-08 15:00:09 +0530

    Learning about Dictionary

--- python_learning.py
+++ python_learning.py
@@ -97,3 +97,30 @@ else:
         
 
 
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+}
+
+print(programming_dictionary["Bug"])
+
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+
+print(programming_dictionary)
+
+empty_dictionary = {}
+
+#Wipe an existing dictionary
+# programming_dictionary={}
+# print(programming_dictionary)
+
+#Edit an item in a dictionary
+print(programming_dictionary["Bug"])
+programming_dictionary["Bug"] = "A moth in your computer"
+print(programming_dictionary["Bug"])
+
+
+#loop through a dictionary
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
