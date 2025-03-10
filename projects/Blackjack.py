31c30ee96e43e9fa484bcd25cfd1fee887b98ce0
dhananjaydogra <dhananjay27dogra@gmail.com>
2025-03-10 23:50:11 +0530

    Added a playable black jack with game flow

--- projects/Blackjack.py
+++ projects/Blackjack.py
@@ -1,10 +1,12 @@
+import random
+
 logo='''
 .------.            _     _            _    _            _    
 |A_  _ |.          | |   | |          | |  (_)          | |   
 |( \/ ).-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __
 | \  /|K /\  |     | '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
 |  \/ | /  \ |     | |_) | | (_| | (__|   <| | (_| | (__|   < 
-`-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\
+`-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\ 
       |  \/ K|                            _/ |                
       `------'                           |__/           
       
@@ -17,23 +19,42 @@ user_input=''
 while game_continue:
     print(logo)
     
-    player_hand = []
+    player_hand =[]
     computer_hand = []
     player_score=0
     computer_score=0
 
+    player_hand.append(random.choice(cards))
+    player_hand.append(random.choice(cards))
+    player_score=sum(player_hand)
+
+    computer_hand.append(random.choice(cards))
+    computer_hand.append(random.choice(cards))
+    computer_score=sum(computer_hand)
+
     print(f"Your cards: {player_hand}, current score: {player_score}")
 
     print(f"Computer's first card: {computer_hand[0]}")
-
-    round_input=input("Type 'y' to get another card, type 'n' to pass: ").lower
-
+    next_card= True
+    while next_card:
+        round_input=input("Type 'y' to get another card, type 'n' to pass: ").lower()
+        if round_input == 'y':
+            player_hand.append(random.choice(cards))
+            player_score=sum(player_hand)
+        else:
+            next_card= False
 
     print(f"Your final hand: {player_hand}, final score: {player_score}")
     print(f"Computer's final hand: {computer_hand}, final score: {computer_score}")
 
+    if computer_score > player_score or player_score > 21:
+        print("You lose 😤")
+    elif player_score > computer_score or computer_score > 21:
+        print("You win 😁")
+    else:
+        print("It's a draw")
 
-    game_input=input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+    game_input=input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
     if game_input == "n":
         game_continue=False
 
